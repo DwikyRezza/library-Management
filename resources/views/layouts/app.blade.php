0ff41aaa6091f142cfb1bfbbaaa9b7c5f8385f06
Dwiky rezza <dwikyrezza33@gmail.com>
2026-06-12 17:44:02 +0700

    feat: add Google Search Console verification meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="MhFD60EegEXVl4JXdyeVrp_jVi5OuUl-Bkgn50w5p9U" />
     <link rel="icon" type="image/png" href="{{ asset('images/logo-libraflow.png') }}">
     <title>@yield('title', 'Lyrary')</title>
     <script>
